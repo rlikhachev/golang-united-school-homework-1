3b95826227a663c4a3899f174a6b13c8fe4d54cb
rlikhachev <rlikhachev@gmail.com>
2022-02-13 22:46:14 +0300

    rm print to prompt

--- solution_test.go
+++ solution_test.go
@@ -1,17 +1,14 @@
 package solution
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/kyokomi/emoji/v2"
 )
 
 func TestGetMessage(t *testing.T) {
-	message := GetMessage()
-	fmt.Println(message) 
+	message := GetMessage() 
 	if message != emoji.Sprint("Hello, :world_map:!") {
 		t.Error("Incorrect message!")
 	}
-	
 }
